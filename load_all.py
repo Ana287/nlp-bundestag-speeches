7512ea90f2037ac5235fd42546e2e5346db59a10
Ana Calotescu <57093103+Ana287@users.noreply.github.com>
2023-08-15 18:53:49 +0200

    fixed setting of unique speaker ids

--- load_all.py
+++ load_all.py
@@ -7,6 +7,10 @@ import re
 df_gp = gpp.get_data()
 df_od = odp.get_data()
 
+# method for removing non-alphabetic characters from strings
+def clean_names(input_string):
+    return re.sub(r'P{L}', '', input_string)
+
 # merge dataframes
 merged_df = pd.concat([df_od, df_gp], ignore_index=True)
 
@@ -18,12 +22,8 @@ merged_df = merged_df.reset_index(drop=True)
 # replace double occurences of the same speaker and unknown genders
 merged_df['speaker_name'] = merged_df['speaker_name'].replace({'Kersten Naumann': 'Kersten Steinke', 'Cajus Julius Caesar': 'Cajus Caesar', 'Matern von Marschall von Bieberstein': ' Matern von Marschall'})
 
-# set unqiue speaker IDs
-merged_df['speaker_id'] = merged_df.groupby('speaker_name').ngroup() + 1
-
 # get rid of mostly_male and mostly_female gender classification
-merged_df['speaker_gender'] = merged_df['speaker_gender'].replace('mostly_male', 'male')
-merged_df['speaker_gender'] = merged_df['speaker_gender'].replace('mostly_female', 'female')
+merged_df['speaker_gender'] = merged_df['speaker_gender'].replace({'mostly_male': 'male', 'mostly_female': 'female'})
 
 # fix faulty gender classification due to whitespace characters
 unknown_rows = merged_df[merged_df['speaker_gender'] == 'unknown']
@@ -33,6 +33,15 @@ for index, row in unknown_rows.iterrows():
 
     merged_df.loc[index, 'speaker_name'] = spaced_capitals
     merged_df.loc[index, 'speaker_gender'] = gc.get_gender(row['speaker_name'].split()[0])
+    
+# clean speaker name strings
+merged_df['speaker_name'] = merged_df['speaker_name'].str.lower().apply(clean_names)
+
+# set unqiue speaker IDs based on lowercase & cleaned speaker names
+merged_df['speaker_id'] = merged_df.groupby('speaker_name').ngroup() + 1
+
+# convert speaker_names to title case again
+merged_df['speaker_name'] = merged_df['speaker_name'].str.title()
 
 # save dataframe to a pickled file
 merged_df.to_pickle('data_merged.pkl')
